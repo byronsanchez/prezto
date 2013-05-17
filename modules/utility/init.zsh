7d16d1c446488763d75227e0bf60024274424178
Pig Monkey <pm@pig-monkey.com>
2013-05-17 11:04:32 -0700

    Personalize ls aliases
    I tried to use the defaults, but I'm picky.

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -91,16 +91,15 @@ else
   fi
 fi
 
-alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
-alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
-alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
-alias lx='ll -XB'        # Lists sorted by extension (GNU only).
-alias lk='ll -Sr'        # Lists sorted by size, largest last.
-alias lt='ll -tr'        # Lists sorted by date, most recent last.
-alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
-alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
+alias l='ls -lh'         # Lists human readable sizes.
+alias ll='l -A'          # Lists human readable sizes, hidden files.
+alias lr='l -R'          # Lists human readable sizes, recursively.
+alias lm='ll | "$PAGER"' # Lists human readable sizes, hidden files through pager.
+alias lx='l -XB'         # Lists sorted by extension (GNU only).
+alias lk='l -Sr'         # Lists sorted by size, largest last.
+alias lt='l -tr'         # Lists sorted by date, most recent last.
+alias lc='l -c'          # Lists sorted by date, most recent last, shows change time.
+alias lu='l -u'          # Lists sorted by date, most recent last, shows access time.
 alias sl='ls'            # I often screw this up.
 
 # Mac OS X Everywhere
