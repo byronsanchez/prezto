a325c9c51dc0f0d63b5a98d157ecb5fe587bc688
Byron Sanchez <bfs5030@gmail.com>
2017-05-31 22:56:34 -0400

    Update encfs mounting shortcuts to work on windows/encfs4win

--- modules/encfs/init.zsh
+++ modules/encfs/init.zsh
@@ -4,14 +4,31 @@
 
 # Check if the encfs filesystem is currently mounted.
 encfsstatus() {
-    cat /etc/mtab | grep -q "^encfs $ENCFSMOUNT"
+    if [[ "$OSTYPE" == cygwin* ]]; then
+        cat /etc/mtab | grep -q "encfs.exe"
+    else
+        cat /etc/mtab | grep -q "^encfs $ENCFSMOUNT"
+    fi
 }
 
 # Mount the encfs filesystem, if not already mounted.
 encfsmount() {
     encfsstatus
     if [ $? -ne 0 ]; then
-        gpg --batch -q -d $ENCFSPASS | encfs --stdinpass $ENCFSCRYPT $ENCFSMOUNT
+
+        if [[ "$OSTYPE" == cygwin* ]]; then
+            # Windows cygwin has to convert cygwin paths to windows path so 
+            # GPG4Win can read it
+            export WINDOWSENCFSPASS=`cygpath -am ${ENCFSPASS}`
+            export WINDOWSENCFSCRYPT=`cygpath -am ${ENCFSCRYPT}`
+            # We have to mount directly to a drive letter in Windows to prevent 
+            # encfs4windows from having file/folder not found issues
+            export WINDOWSENCFSMOUNT="X:/"
+            gpg --batch -q -d $WINDOWSENCFSPASS | encfs --stdinpass $WINDOWSENCFSCRYPT $WINDOWSENCFSMOUNT
+        else
+            gpg --batch -q -d $ENCFSPASS | encfs --stdinpass $ENCFSCRYPT $ENCFSMOUNT
+        fi
+
     fi
 }
 
@@ -19,7 +36,13 @@ encfsmount() {
 encfsdismount() {
     encfsstatus
     if [ $? -eq 0 ]; then
-        fusermount -u $ENCFSMOUNT
+
+        if [[ "$OSTYPE" == cygwin* ]]; then
+            Sudo dokanctl /u X
+        else
+            fusermount -u $ENCFSMOUNT
+        fi
+
     fi
 }
 
