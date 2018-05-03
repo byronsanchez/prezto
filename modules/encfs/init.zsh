50b72e1ff8387c5b86bffd3f4484714352dea0f2
Byron Sanchez <byron@byronsanchez.io>
2018-05-03 11:54:37 -0400

    Update encfsmount to use  gpg2 for compatibility

--- modules/encfs/init.zsh
+++ modules/encfs/init.zsh
@@ -24,9 +24,12 @@ encfsmount() {
             # We have to mount directly to a drive letter in Windows to prevent 
             # encfs4windows from having file/folder not found issues
             export WINDOWSENCFSMOUNT="X:/"
-            gpg --batch -q -d $WINDOWSENCFSPASS | encfs --stdinpass $WINDOWSENCFSCRYPT $WINDOWSENCFSMOUNT
+            # Use gpg2 to be more compatible (Fedora's gpg == 1 vs. explicit
+            # gpg2 isss NECESSARY); use gpg shell aliases for systems that don't have
+            # an explicit gpg2
+            gpg2 --batch -q -d $WINDOWSENCFSPASS | encfs --stdinpass $WINDOWSENCFSCRYPT $WINDOWSENCFSMOUNT
         else
-            gpg --batch -q -d $ENCFSPASS | encfs --stdinpass $ENCFSCRYPT $ENCFSMOUNT
+            gpg2 --batch -q -d $ENCFSPASS | encfs --stdinpass $ENCFSCRYPT $ENCFSMOUNT
         fi
 
     fi
